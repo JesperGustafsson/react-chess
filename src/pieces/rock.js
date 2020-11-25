141ee850149986d038a4bacf8fe80166a1cbed25
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2020-11-25 13:38:44 +0100

    REEEEE

--- src/pieces/rock.js
+++ src/pieces/rock.js
@@ -1,7 +1,7 @@
 import Obstacle from './obstacle.js';
 export default class Rock extends Obstacle {
     constructor(player){
-        super(player,"rock.png");
-        //super("https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg");
+        //super(player,"react-chess/src/pieces/rock.png");
+        super(player, "http://t3.gstatic.com/images?q=tbn:ANd9GcReN9KWqKEGl0uRkdBWgcl-LaLK_8ecIzuBAhVBx9H82E40amLbEMYrZTc5fsgf4jxEpbAJaaV5znMbXHevmM0");
     }
 }
